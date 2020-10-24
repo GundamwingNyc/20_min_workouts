0df038b5249afa2fcc702c081a4d31ce37def9ab
RAMSEY <git sttatus>
2020-10-24 16:42:50 -0400

    commit workout site

---
+++ or/script.js
@@ -0,0 +1,4 @@
+let foo = 42;    // foo is now a number
+foo     = 'bar'; // foo is now a string
+foo     = true;  // foo is now a boolean
+
---
+++ script.js
@@ -0,0 +1,9 @@
+const names = ["Brooklyn", "Queens", "Bronx", "Manhattan"];
+console.log(names[1]);
+/*console.log(names.length);
+
+for (const el of names) {
+  console.log(el);
+}
+names.push("julie");
+const julieIndex = names.findIndex((el) => el === "julie");
